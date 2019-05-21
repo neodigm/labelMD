a941b48e98d7f95608485b30bb3919dd70afe1bc
neodigm <neodigm@users.noreply.github.com>
2019-05-21 09:48:38 -0500

    content

--- dist/labelMD.js
+++ dist/labelMD.js
@@ -1,4 +1,4 @@
-console.clear();
+//console.clear();
 var labelMD_default = {  //  Detault configuration
     mode: "default",
     aExcludeID: ["js-inp-search--id","js-toplogo-slide__input--id","js-inp-topsearch--id","quickSearch-query-for-small","emailSubscribeAddressModal","emailAddressFieldId","quickSearch-query"],
@@ -11,19 +11,21 @@ var labelMD = ( function( _d, _g ){  //  Dynamic Material Design INPUT Labels
     var aInp = [], oCnf = {}, sBrand;
     oCnf = ( typeof _g.labelMD_custom === "undefined") ? _g.labelMD_default : _g.labelMD_custom;
     function onInpFoc( _el ){
-        console.log("yikes f| " + _el.placeholder );
-        _el.dataset.labelMdCl = _el.className;
-        _el.className = "";
+        if( typeof _el.dataset.labelMd !== "undefined" ){
+            _el.dataset.labelMdCl = _el.className;
+            _el.className = "";            
+        }
     }
     function onInpBlur( _el ){
-        console.log("yikes b| " + _el.placeholder );
-        _el.className = _el.dataset.labelMdCl;
+        if( typeof _el.dataset.labelMd !== "undefined" ){
+            _el.className = _el.dataset.labelMdCl;
+        }
     }
     return {
         bind: function( _sBrand = "LTD" ){
             sBrand = _sBrand;
             aInp = [].slice.call( _d.querySelectorAll("INPUT") ).filter(function( _inp ){
-                var bRet = true;  //  Exclusion logic
+                var bRet = true;  //  Exclusion logic  //  TODO exclude if no placeholder or len LT 4
                 if( "checkbox hidden image submit".indexOf( _inp.type ) !== -1 ) return false;
                 oCnf.aExcludeCL.map( function( _cl ){
                     if( _inp.classList.toString().indexOf( _cl ) !== -1 ) bRet = false;
@@ -35,8 +37,11 @@ var labelMD = ( function( _d, _g ){  //  Dynamic Material Design INPUT Labels
                 _inp.addEventListener("focus", function( _ev ){ onInpFoc(_ev.currentTarget); });
                 _inp.addEventListener("blur", function( _ev ){ onInpBlur(_ev.currentTarget); });
             } );
+        },
+        unbind: function( sQuery ){
+            //  remove data attr but leave in data model
         }
     };
 } )( document, window );
 
-labelMD.bind( "LTD" );
+labelMD.bind( document.body.dataset.brand );
