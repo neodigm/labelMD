ba6171ba30fbbb4c31bc732659581e50283c0cac
neodigm <neodigm@users.noreply.github.com>
2019-05-25 19:12:00 -0500

    content

--- dist/labelMD.js
+++ dist/labelMD.js
@@ -1,38 +1,51 @@
 //console.clear();
 var labelMD_default = {  //  Detault configuration
-    mode: "default",
-    aExcludeID: ["js-inp-search--id","js-toplogo-slide__input--id","js-inp-topsearch--id","quickSearch-query-for-small","emailSubscribeAddressModal","emailAddressFieldId","quickSearch-query"],
+    mode: "default", //  QQQQ REMOVE firstName from below
+    aExcludeID: ["firstName", "js-inp-search--id","js-toplogo-slide__input--id","js-inp-topsearch--id","quickSearch-query-for-small","emailSubscribeAddressModal","emailAddressFieldId","quickSearch-query"],
     aExcludeCL: ["js-eml__input--field", "js-sms__input"]
 };
 
 //var labelMD_custom = { mode: "custom" }  //  Custom configuration
 
 var labelMD = ( function( _d, _g ){  //  Dynamic Material Design INPUT Labels
-    var aInp = [], oCnf = {}, sBrand;
-    oCnf = ( typeof _g.labelMD_custom === "undefined") ? _g.labelMD_default : _g.labelMD_custom;
+    var aInp = [], aLab = [], oCnf = {}, sBrand;
+    oCnf = ( typeof _g.labelMD_custom === "undefined") ? _g.labelMD_default : _g.labelMD_custom; // over-ride
     function onInpFoc( _el ){
         if( typeof _el.dataset.labelMd !== "undefined" ){
-            //_el.dataset.labelMdCl = _el.className;
-            //_el.className = "label-md__inp--ltd";            
+            _el.dataset.labelMdPh = _el.placeholder;
+            _el.placeholder = "";
+            aLab[ _el.dataset.labelMd ].classList.remove( "h-ds__none" );
         }
     }
     function onInpBlur( _el ){
         if( typeof _el.dataset.labelMd !== "undefined" ){
-            //_el.className = _el.dataset.labelMdCl;
+            if( typeof _el.dataset.labelPh !== "undefined" ){
+                _el.placeholder = _el.dataset.labelMdPh;
+                aLab[ _el.dataset.labelMd ].classList.add( "h-ds__none" );
+            }
         }
+    }
+    function parsePH( sPH ){
+        return sPH.replace("* ", "");
     }
     return {
         bind: function( _sBrand = "LTD" ){
             sBrand = _sBrand.toLowerCase();
             aInp = [].slice.call( _d.querySelectorAll("INPUT") ).filter(function( _inp ){
-                var bRet = true;  //  Exclusion logic  //  TODO exclude if no placeholder or len LT 4
+                var bRet = true;  //  Exclusion logic  //  TODO exclude if no placeholder or len LT 4, or no id
                 if( "checkbox hidden image submit".indexOf( _inp.type ) !== -1 ) return false;
-                oCnf.aExcludeCL.map( function( _cl ){
+                oCnf.aExcludeCL.map( function( _cl ){  //  Exclude by Class
                     if( _inp.classList.toString().indexOf( _cl ) !== -1 ) bRet = false;
                 } );
                 return ( bRet && (oCnf.aExcludeID.indexOf( _inp.id ) === -1) );
             });
             aInp.map( function( _inp, _idx ){
+                let eLab = _d.createElement("LABEL");
+                eLab.textContent = parsePH( _inp.placeholder );
+                eLab.setAttribute("for", _inp.id);
+                eLab.classList.add("label-md__lab", "h-ds__none");
+                aLab.push( eLab );
+                _inp.before( eLab );  //  TODO test in MS IE & Edge
                 _inp.dataset.labelMd = _idx;
                 _inp.className = "label-md__inp";
                 _inp.dataset.labelMdCl = _inp.className;
