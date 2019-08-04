33c50e733bfb6ac976270fcd76534f17153fba3c
neodigm <neodigm@users.noreply.github.com>
2019-08-04 14:52:16 -0500

    content

---
+++ evocart/material_design_page.js
@@ -0,0 +1,110 @@
+var mdPage = ( function( _d ){  //  Material Design INPUT Labels
+    var aInp = [], aLab = [], sBrand;
+    var oCSS = [
+[".h-vs__hidden", ["visibility", "hidden"]],
+["input.label-md__inp", [
+    ["background-color", "#fff"],
+    ["border-radius", "4px"],
+    ["box-shadow", "none"],
+    ["color", "#343a40"],
+    ["display", "block"],
+    ["font-size", "16px"],
+    ["height", "44px"],
+    ["padding", "6px"],
+    ["z-index", "1"]
+]],
+["label.label-md__lab", [
+    ["background-color", "#fff"],
+    ["cursor", "text"],
+    ["display", "inline"],
+    ["font-family", "sans-serif"],
+    ["font-size", "14px"],
+    ["left", "6px"],
+    ["margin", "0 0 0 8px"],
+    ["padding", "0 6px"],
+    ["position", "relative"],
+    ["top", "10px"],
+]],
+["body[data-brand=LS]  input.label-md__inp", [
+    ["border", "1px solid #a1b5c2"]
+]],
+["body[data-brand=LTD] input.label-md__inp", [
+    ["border", "1px solid #30619a"]
+]],
+["body[data-brand=LS]  input:focus.label-md__inp", [
+    ["border", "1px solid #343a40"]
+]],
+["body[data-brand=LTD] input:focus.label-md__inp", [
+    ["border", "1px solid #4a90e2"]
+]],
+["body[data-brand=LS]  label.label-md__lab", [
+    ["color", "#343a40"]
+]],
+["body[data-brand=LTD] label.label-md__lab", [
+    ["color", "#4a90e2"]
+]],
+];
+    function onInpFoc( _el ){  //  focus
+        if( typeof _el.dataset.mdLabel !== "undefined" ){
+            var sPH = _el.getAttribute("placeholder");
+            if( sPH ){
+                _el.dataset.mdLabelPh = sPH;
+                _el.placeholder = "";
+                aLab[ _el.dataset.mdLabel ].classList.remove( "h-vs__hidden" );                
+            }
+        }
+    }
+    function onInpBlur( _el ){  //  blur
+        if( typeof _el.dataset.mdLabel !== "undefined" ){
+            if( typeof _el.dataset.mdLabelPh !== "undefined" ){
+                _el.placeholder = _el.dataset.mdLabelPh;
+                aLab[ _el.dataset.mdLabel ].classList.add( "h-vs__hidden" );
+            }
+        }
+    }
+    function parsePH( sPH ){
+        return sPH.replace("* ", "");
+    }
+    function addStylesheetRules(rules) {
+        var styleEl = document.createElement("style");
+        document.head.appendChild(styleEl);
+        var styleSheet = styleEl.sheet;
+        for (var i = 0; i < rules.length; i++) {
+            var j = 1, 
+            rule = rules[i], 
+            selector = rule[0], 
+            propStr = "";
+            if (Array.isArray(rule[1][0])) {
+                rule = rule[1];
+                j = 0;
+            }
+            for (var pl = rule.length; j < pl; j++) {
+                var prop = rule[j];
+                propStr += prop[0] + ": " + prop[1] + " !important;\n";
+            }
+            styleSheet.insertRule(selector + "{" + propStr + "}", styleSheet.cssRules.length);
+        }
+    }
+    return {
+        bind: function( _sBrand = "LTD" ){  //  Wire Events
+            addStylesheetRules( oCSS );
+            sBrand = _sBrand.toLowerCase();
+            aInp = [].slice.call( _d.querySelectorAll( "[data-md-page]") );
+            aInp.map( function( _inp, _idx ){
+                let eLab = _d.createElement("LABEL");
+                eLab.textContent = parsePH( _inp.placeholder );
+                eLab.setAttribute("for", _inp.id);
+                eLab.classList.add("label-md__lab", "h-vs__hidden");
+                aLab.push( eLab );
+                _inp.before( eLab );
+                _inp.dataset.mdLabel = _idx;
+                _inp.dataset.mdLabelCl = _inp.className;
+                _inp.classList.add( "label-md__inp" );  
+                _inp.addEventListener("focus", function( _ev ){ onInpFoc(_ev.currentTarget); });
+                _inp.addEventListener("blur", function( _ev ){ onInpBlur(_ev.currentTarget); });
+            } );
+        }
+    };
+} )( document );
+
+mdPage.bind( document.body.dataset.brand );
--- evocart/signin_popup.js
+++ evocart/signin_popup.js
@@ -1,22 +1,48 @@
 "use strict"
-var oSignIn = (function( dc, sQuery ){
-  var eForgotLink;
+var oSignIn = (function( dc, sQuery, sIDs ){  //  Sign-In and I Forgot Modal Validation Logic (from Cart)
+  var eForgotLink = dc.querySelectorAll( sQuery ), bForgotLink = false;  //  Modal State
+  var eC2A_save = dc.getElementById( sIDs[0] ), eC2A_reset = dc.getElementById( sIDs[1] );  //  Buttons
+  var eSI_email = dc.getElementById( sIDs[2] ), eSI_pw = dc.getElementById( sIDs[3] );  //  INPUT sign in
+  var eIF_email = dc.getElementById( sIDs[4] );  //  INPUT I forgot
   return {
     "bind": function(){
-      eForgotLink = dc.querySelectorAll( sQuery );
-      if( eForgotLink ){
+      if( eForgotLink ){  //  Show Sign-In UI with Forgot link.
         eForgotLink[1].addEventListener( "click", function( ev ){
+          bForgotLink = true;
           eForgotLink[0].classList.add("hide");
           eForgotLink[2].classList.remove("hide");
         });
+        if( eC2A_save && eC2A_reset && eSI_email && eSI_pw && eIF_email ){
+          [ eC2A_save, eC2A_reset ].forEach( function( el ){  //  Bind event to buttons
+            el.addEventListener("click", function( ev ){
+              var aMsg = ["","Please Enter Your|Email Address", "Please Enter Your|Password"], nMsg = 0;
+              if( bForgotLink ){
+                if( eIF_email.value === "" ) nMsg = 1;
+              }else{
+                if( eSI_pw.value === "" ) nMsg = 2;
+                if( eSI_email.value === "" ) nMsg = 1;
+              }
+              if( nMsg ){
+                if( typeof ltdc_snackbar !== "undefined" ) ltdc_snackbar.q( aMsg[ nMsg ], "error" );
+              }else{
+                if( bForgotLink ){  //  Form is valid
+// ADD SUBMIT FORGOT LOGIC HERE
+                }else{
+// ADD SUBMIT SIGN IN LOGIC HERE
+                }
+              }
+            });
+          });
+        }
       }
     },
-    "signin": function(){
-      if( eForgotLink ){
+    "signin": function(){  //  This should be called whenever the modal is opened.
+      if( eForgotLink ){  // Init state is Sign-In (not I forgot).
+        bForgotLink = false;
         eForgotLink[0].classList.remove("hide");
         eForgotLink[2].classList.add("hide");
       }
     }
   }
-})( document, ".js-forgot__view");
+})( document, ".js-forgot__view", ["js-sign-in__save", "js-forgot__reset", "js-sign-in__email", "js-sign-in__password", "js-forgot__email"]);
 oSignIn.bind();
