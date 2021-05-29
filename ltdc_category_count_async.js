ba3fa07df7393fadebbf48d79518c2a2cdc3efda
Scott C. Krause <3151842+neodigm@users.noreply.github.com>
2021-05-29 11:26:23 -0500

    🌑🌒🌓🌔🌕🌖🌗🌘🌑

--- ltdc_category_count_async.js
+++ ltdc_category_count_async.js
@@ -1,14 +1,12 @@
-var catcount = (function(_d,_q){  //  Report on number of products by category | Scott C. Krause 2020 - see gist
-    "use strict";
+var catcount = (function(_d,_q){  //  Report on number of products by category | Scott C. Krause 2022 - see gist
     var aSub = [];
 
-    
     return {
         init: function(){
             aSub = [].slice.call( _d.querySelectorAll( _q ) ).filter(function( el ){
                 return (el.href && (el.href.indexOf("browse") !== -1));
             });
-        } }
+        } }  //  🌑🌒🌓🌔🌕🌖🌗🌘🌑
 
 })(document, "#main-content .sub-cat > a");
 catcount.init();
